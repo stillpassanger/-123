4f80d481f12cb713d2df293cd427a2aebccb3781
fengqilin <376807727@qq.com>
2016-03-12 17:39:16 +0800

    dfdsfd

--- 合并和分支/合并和分支/ViewController.m
+++ 合并和分支/合并和分支/ViewController.m
@@ -19,6 +19,8 @@
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"345");
     NSLog(@"23345");
+    NSLog(@"32434");
+    NSLog(@"34343");
 }
 
 - (void)didReceiveMemoryWarning {
